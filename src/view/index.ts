358d52744eb613b6e25887c0e866c8e3c231bab0
Alberto Mijares <almilo@almilo.es>
2016-08-09 15:48:19 +0200

    (docs): improve naming and comments

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,17 +1,17 @@
 import { Observable } from 'rxjs/Rx';
 import { State } from '../state/state';
-import headerView from './header-view';
-import bodyView from './body-view';
-import footerView from './footer-view';
+import header from './header-view';
+import body from './body-view';
+import footer from './footer-view';
 import { versioned } from '../lib/index';
 
 export default function (states: Observable<State>): Observable<string> {
     return Observable.combineLatest( // combine all the views into the main view using the most up-to-date values
-        versioned(headerView(states), 'header'),
-        versioned(bodyView(states), 'body'),
-        versioned(footerView(states), 'footer'),
+        versioned(header(states), 'header'),
+        versioned(body(states), 'body'),
+        versioned(footer(states), 'footer'),
         (header, body, footer) => `${header}\n${body}\n${footer}`
     )
-        .sample(Observable.interval(100)) // debounce the combined view to avoid flickering
+        .sample(Observable.interval(100)) // sample the combined view to prevent redundant rendering cycles
         .share(); // do not set up different processing pipelines
 }
