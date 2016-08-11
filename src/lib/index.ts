d33f3c89378f7c1d703e5f20ee05ed070eea5309
Alberto Mijares <almilo@almilo.es>
2016-08-11 20:42:00 +0200

    (refactor): remove dead code (select and versioned functions)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,57 +1,6 @@
 import { Observable } from 'rxjs/Rx';
 import { Action } from '../state/actions';
 
-interface Equality<T> {
-    (value1: T, value2: T): boolean
-}
-
-export function select<T, V>(observable: Observable<T>, selector?: string | Function, equality: Equality<T> = shallowEquals): Observable<V> {
-    return observable
-        .map(asFunction(selector)) // select the part of the application state in which we are interested
-        .distinctUntilChanged(equality); // don't emmit if the value we are interested in doesn't change
-
-    function asFunction(selector: string | Function) {
-        return (value: any) => {
-            if (!selector) {
-                return value;
-            }
-
-            return selector instanceof Function ? selector(value) : getPath(value, selector);
-        };
-
-        function getPath(obj: Object, path: string): any {
-            return path.split('.').reduce((source, property) => (<{[key: string]: any}>source)[property], obj);
-        }
-    }
-}
-
-function shallowEquals(value1: any, value2: any): boolean {
-    if (value1 instanceof Array && value2 instanceof Array) {
-        return value1.length === value2.length && value1.every((item: any, index: number) => value1[index] === value2[index]);
-    } else if (value1 instanceof Object && value2 instanceof Object) {
-        const properties1 = Object.keys(value1);
-        const properties2 = Object.keys(value2);
-
-        return properties1.length === properties2.length && properties1.every(hasCounterpart) && properties2.every(hasCounterpart);
-
-        function hasCounterpart(propertyName: string) {
-            return (<{[key: string]: any}>value1)[propertyName] === (<{[key: string]: any}>value2)[propertyName];
-        }
-    } else {
-        return value1 === value2;
-    }
-}
-
-const versions: {[index: string]: number} = {};
-
-export function versioned(views: Observable<string>, id: string): Observable<string> {
-    return views.map(view => {
-        versions[id] = (versions[id] !== undefined ? versions[id] : -1) + 1;
-
-        return `<div class="debug">${view}<span class="version">version: ${versions[id]}&nbsp;</span></div>`;
-    });
-}
-
 interface ObservableFactory<T> {
     (initialState: T, actions: Observable<Action>): Observable<T>
 }
@@ -85,4 +34,25 @@ interface Reducer<T> {
 
 export function toObservableFactory<T>(reducer: Reducer<T>, equality: Equality<T> = shallowEquals): ObservableFactory<T> {
     return (initialState: T, actions: Observable<Action>) => actions.scan(reducer, initialState).distinctUntilChanged(equality);
+}
+
+interface Equality<T> {
+    (value1: T, value2: T): boolean
+}
+
+function shallowEquals(value1: any, value2: any): boolean {
+    if (value1 instanceof Array && value2 instanceof Array) {
+        return value1.length === value2.length && value1.every((item: any, index: number) => value1[index] === value2[index]);
+    } else if (value1 instanceof Object && value2 instanceof Object) {
+        const properties1 = Object.keys(value1);
+        const properties2 = Object.keys(value2);
+
+        return properties1.length === properties2.length && properties1.every(hasCounterpart) && properties2.every(hasCounterpart);
+
+        function hasCounterpart(propertyName: string) {
+            return (<{[key: string]: any}>value1)[propertyName] === (<{[key: string]: any}>value2)[propertyName];
+        }
+    } else {
+        return value1 === value2;
+    }
 }
